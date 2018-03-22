e0b65c545ae0f8826118a8270557857071d78005
daleryon <35710528+daleryon@users.noreply.github.com>
2018-03-21 22:12:37 -0400

    Items created for future cart.

--- src/cartitems/Hats.java
+++ src/cartitems/Hats.java
@@ -4,16 +4,13 @@ public class Hats {
 	private int id;
 	private String name;
 	private double cost;
-	private String description;
 	private int quantity;
 	private double tax;
 
-	public Hats(int itemID, String itemName, double itemCost, String itemDescription, int itemQuantity,
-			double itemTax) {
+	public Hats(int itemID, String itemName, double itemCost, int itemQuantity, double itemTax) {
 		setId(itemID);
 		setName(itemName);
 		setCost(itemCost);
-		setDescription(itemDescription);
 		setQuantity(itemQuantity);
 	}
 
@@ -39,14 +36,6 @@ public class Hats {
 
 	public void setCost(double cost) {
 		this.cost = cost;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
 	}
 
 	public int getQuantity() {
